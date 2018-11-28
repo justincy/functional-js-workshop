f7d7635cae16eca8856f9955805fe4f2509e0ef5
Justin York <justinyork@familysearch.org>
2018-11-28 10:00:06 -0700

    Recursion

--- recursion.js
+++ recursion.js
@@ -1,7 +1,23 @@
-function reduce(arr, fn, initial) {
-  if (arr.length === 0) return initial;
-  const nextValue = fn(initial, arr[0], 0, arr);
-  return reduce(arr.slice(1), fn, nextValue);
+function getDependencies(tree) {
+  // SOLUTION GOES HERE
+  // Note: Feel free to add additional arguments
+  // to this function for use with recursive calls.
+  // Or not! There are many ways to recurse.
+  let deps = [];
+  if (tree && tree.dependencies) {
+    Object.keys(tree.dependencies).forEach(name => {
+      deps.push(`${name}@${tree.dependencies[name].version}`);
+      deps = deps.concat(getDependencies(tree.dependencies[name]));
+    });
+  }
+  deps.sort()
+  let uniqueDeps = [];
+  deps.forEach((dep) => {
+    if (uniqueDeps.indexOf(dep) === -1) {
+      uniqueDeps.push(dep);
+    }
+  })
+  return uniqueDeps
 }
 
-module.exports = reduce
+module.exports = getDependencies
